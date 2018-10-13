a65fb2d53ac7cce0374033658eb4533c51a2864e
Jasper Ginn <jasperginn@gmail.com>
2018-10-13 11:17:13 +0200

    Update documentation

--- R/part1.R
+++ R/part1.R
@@ -1,16 +1,18 @@
-## Functions for part I
+## Functions for assignment 2, part I
 #
 # Functions:
 #  - t_test: exported function that performs a t-test for two variables. Returns an S3 method of class t_test
+#  - print.t_test: S3 print method for 't_test' class
 #  - t_statistic: helper function. Not exported. Returns t-statistic given inputs.
 #  - pooled_variance: helper function. Not exported. Returns pooled variance given inputs.
-#  - print.t_test: S3 print method for 't_test' class
 
-#' Calculate a t-test between two variables
+# Main ----
+
+#' Perform a two-tailed t-test between two variables
 #'
 #' @param x a numeric vector
 #' @param y a numeric vector
-#' @param variance_equal boolean (TRUE/FALSE) indicating if variance of x and y are equal or not.
+#' @param variance_equal boolean (TRUE/FALSE) indicating if variance of x and y are equal or not. If variances are equal, the function will calculate the pooled variance. Else, it will use the formula for unequal variances.
 #'
 #' @author Jasper Ginn
 #'
@@ -91,7 +93,7 @@ t_test <- function(x, y, variance_equal = TRUE) {
   }
 
   # P-value (make this two-tailed)
-  pval <- 2 * pt(tt_result, df, lower.tail = FALSE)
+  pval <- 2 * pt( tt_result, df, lower.tail = FALSE )
 
   ## Make list of results
   res <- list(
@@ -143,7 +145,8 @@ print.t_test <- function(x) {
 
   ## Use paste to create a summary of the data
 
-  msg <- paste0(# Print whether test is equal/unequal variances
+  msg <- paste0(
+    # Print whether test is equal/unequal variances
     "Two-sided t-test with",
     ifelse(x$inputs$variance_equal, " equal ", " unequal "),
     "variances.",
@@ -162,7 +165,8 @@ print.t_test <- function(x) {
     # If equal variances, print pooled variance
     ifelse(x$inputs$variance_equal,
            paste0("\n", "------------------------", "\n",
-                  "Pooled variance: ", round(x$summary_statistics$pooled_variance, digits=2)),
+                  "Pooled variance: ", round(x$summary_statistics$pooled_variance,
+                                             digits=2)),
            ""),
     "\n\n",
     # Print t-statistic, degrees of freedom & p-values
@@ -184,7 +188,7 @@ print.t_test <- function(x) {
 # @param n_x, n_y number of observations for x, y
 # @param ... optional (named) arguments. Accepted arguments are 'poolvar' (for equal variances) or 'var_x' & 'var_y' (for unequal variances)
 #
-# @return t-statistic for
+# @return t-statistic for x, y
 t_statistic <- function(mu_x, mu_y, n_x, n_y, ...) {
 
   # Store optional arguments in list
@@ -214,13 +218,11 @@ t_statistic <- function(mu_x, mu_y, n_x, n_y, ...) {
 
 # Calculate the pooled variance of two variables
 #
-# @param
+# @param var_x, var_y the variances of variables x, y
+# @param n_x, n_y number of observations for x, y
 #
-# @return
-pooled_variance <- function(var_x,
-                            var_y,
-                            n_x,
-                            n_y) {
+# @return pooled variance of variables x, y
+pooled_variance <- function(var_x, var_y, n_x, n_y) {
 
   # Apply the formula for pooled variance
   nom <- (n_x - 1) * var_x + (n_y -1) * var_y
