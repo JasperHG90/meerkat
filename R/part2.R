5aa0f7b12a8ae97e448553032bafae8bb219349e
Jasper Ginn <jasperginn@gmail.com>
2018-10-16 10:15:44 +0200

    Add skeleton for linear model

---
+++ R/part2.R
@@ -0,0 +1,37 @@
+## Functions for assignment 2, part II
+#
+# Functions:
+
+# Main functions ----
+
+linear_model <- function(data) {
+
+
+
+}
+
+# Helper functions ----
+
+summary_statistics <- function(data) {
+
+
+  # Return list
+
+}
+
+linear_regression <- function(X, y) {
+
+
+
+}
+
+print.linear_model <- function(x) {
+
+
+}
+
+plot.linear_model <- function(x) {
+
+
+
+}
