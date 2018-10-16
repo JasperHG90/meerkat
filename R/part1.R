ab379cd198fd3c3ebc0e11750aab7822e367400d
Jasper Ginn <jasperginn@gmail.com>
2018-10-16 10:15:56 +0200

    Add bootstrapping function for t-test

--- R/part1.R
+++ R/part1.R
@@ -231,3 +231,59 @@ pooled_variance <- function(var_x, var_y, n_x, n_y) {
   return(nom/denom)
 
 }
+
+# Use bootstrapping to calculate 95% confidence interval
+#
+# @param
+#
+# @return
+bootstrap <- function(x, y, R = 1000, ssize = 0.5) {
+
+  ## ssize must be less than 1 & larger than 0
+  if(!(ssize < 1 & ssize > 0)) {
+
+    stop("ssize must be between 0 and 1")
+
+  }
+
+  ## Open output vector
+  tstat_out <- vector(length = R)
+
+  ## Loop through iterations
+  for(it in 1:R) {
+
+    ## Sample sizes --> round up
+    ss_x <- ceiling(ssize * length(x))
+    ss_y <- ceiling(ssize * length(y))
+
+    s_x <- sample(x, ss_x, replace = TRUE)
+    s_y <- sample(y, ss_y, replace = TRUE)
+
+    ## Perform test
+    test <- t_test(s_x, s_y)
+    tstat_out[it] <- test$test$tstat
+
+  }
+
+  ## Sort the values
+  tstat_out_sorted <- sort(tstat_out)
+
+  ## Alpha value
+  a <- 0.05
+
+  ## Get lower and upper values of the 95% CI
+  lower_ci_ind <- (1/2) * a * R
+  upper_ci_ind <- 1 + (1 - (1/2) * a) * R
+
+  lower_ci <- tstat_out_sorted[lower_ci_ind]
+  upper_ci <- tstat_out_sorted[upper_ci_ind]
+
+  ## Return
+  return(
+    list(
+      "lower" = lower_ci,
+      "upper" = upper_ci
+    )
+  )
+
+}
